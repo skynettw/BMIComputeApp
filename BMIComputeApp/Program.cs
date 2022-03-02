1ce83d98eb59a923e5a19cb47f90052412716616
skynettw <skynet.tw@gmail.com>
2022-03-02 21:44:30 +0800

    加入專案檔案。

---
+++ BMIComputeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BMIComputeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
